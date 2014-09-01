6a98d6bac81c30f5cc73e0b13bc1d720c1be916a
Matt Travis <matt.travis1@gmail.com>
2014-09-01 12:48:13 +0100

    Removed need for web service
    Added xy_osgb script to convert to grid ref

--- PointTool.py
+++ PointTool.py
@@ -1,8 +1,10 @@
 import urllib
+import math
 from qgis.gui import *
 from qgis.core import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from xy_to_osgb import *
 class PointTool(QgsMapTool):
     def __init__(self, canvas):
         QgsMapTool.__init__(self, canvas)
@@ -24,24 +26,23 @@ class PointTool(QgsMapTool):
         #Get the click
         x = event.pos().x()
         y = event.pos().y()
-        
-        espg = self.canvas.mapRenderer().destinationCrs().authid()
         bbox = QgsRectangle(4999.99,4999.69,660000.06,1225000.12)
-        
+        #espg = self.canvas.mapRenderer().destinationCrs().authid()
         point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
-        coords = point.toString ()
-        f = urllib.urlopen("http://gridref.longwayaround.org.uk/convert/%s?figures=4" % coords)
-        f.geturl() # Prints the final URL with parameters.
-        gridref = f.read() # Prints the contents
-        os_ref = gridref[12:18]
+        coords = point.toString()
+        print coords
+        easting = int(coords[0:6])
+        northing = int(coords[15:20])
+        #f = urllib.urlopen("http://gridref.longwayaround.org.uk/convert/%s?figures=4" % coords)
+        #f.geturl() # Prints the final URL with parameters.
+        #gridref = f.read() # Prints the contents
+        os_ref = xy_to_osgb(easting,northing)
         print os_ref
-        #if espg == '27700':
         if bbox.contains(point):
             QMessageBox.information(None,"Info", "Grid Ref: " + os_ref)
         else:
             QMessageBox.information(None,"Error", "Point out of bounds")
-        #print espg
-        #print bbox.toString()
+
     
         
         
