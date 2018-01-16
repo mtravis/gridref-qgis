08b708b00352c06852bdc9476e6f7f619004bd29
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-01-16 14:10:10 +0000

    Attempt second unit test
    #16

--- test/test_gridref.py
+++ test/test_gridref.py
@@ -23,6 +23,8 @@ from PyQt4 import QtCore, QtGui, QtTest
 from utilities import get_qgis_app
 
 from xy_to_osgb import xy_to_osgb
+from grid_ref_utils import GridRefException
+
 QGIS_APP = get_qgis_app()
 
 
@@ -35,6 +37,12 @@ class QGISTest(unittest.TestCase):
         osgb_gridref = xy_to_osgb(393618.933445, 564351.935939)
         expected = "NY 93 64"
         self.assertEqual(osgb_gridref, expected)
+
+    def test_xy_out_of_osgb_bounds(self):
+        """Non-OSGB coordinates raises correct exception"""
+
+        osgb_gridref = xy_to_osgb(-10, 0)
+        self.assertRaises(xy_to_osgb(-10, 0), GridRefException)
 
 if __name__ == '__main__':
     unittest.main()
